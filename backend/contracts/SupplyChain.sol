39053a4c14d035f51a321537616ced50999c4c81
SMdG <72032559+Surya-mdg@users.noreply.github.com>
2023-04-21 13:49:21 +0530

    Completed supply chain contract v1.0
    Added all functions required to run a basic version of supply chain & code in test file to test all the functions.

--- backend/contracts/SupplyChain.sol
+++ backend/contracts/SupplyChain.sol
@@ -8,17 +8,18 @@ contract SupplyChain
 {
     uint256 public prodCount;
     uint256 prodIndexOffset = 1000000000;
+    string password = "$2a$12$REJHy2aPASYWEXUdVwjpZu5xbR2OTludsvq4iYoPap/k1UUbo5Sjy";
 
-    mapping(uint256 => string) prodId; //Maps names of products with a unique id
-    mapping(uint256 => Product) prodIndex; //Maps the product code with the respective product
+    mapping(uint256 => Product) prodId; //Maps names of products with a unique id
+    mapping(uint256 => uint256) prodIndex; //Maps the product code with the respective product information
+    mapping(uint256 => bool) prodExist; //Used to check if product code exists or not
     mapping(address => User) users; //Maps the address of a user with their information
-
-    event ProductCreated(uint256 prodIndex); //Event to indicate a new product has been created
+    mapping(uint256 => Node[]) public supplyChain; //Stores the supply chain for each product created
 
     constructor()
     {
-        prodId[0] = "Peanut Butter";
-        prodId[1] = "Mixed Fruit Jam";
+        prodId[0] = Product("Peanut Butter", "Food paste or spread made from ground, dry-roasted peanuts.");
+        prodId[1] = Product("Mixed Fruit Jam", "Made from a mixture of fruits are usually called conserves, especially when they include citrus fruits, nuts, raisins, or coconut.");
     }
 
     struct User //Used to store user information
@@ -33,24 +34,57 @@ contract SupplyChain
     {
         User userInfo;
         string prodName;
-        string prodDesc;
+        string processDesc;
         address nodeAddress;
     }
 
     struct Product //Product's supply chain information is stored here
     {
         string name;
-        Node[] nodes;
+        string desc;
     }
 
-    function CreateProduct(uint256 _id) external //Generates a unique non-random code and maps it with the newly created product
+    function CreateProduct(uint256 _id) external//Generates a unique non-random code and maps it with the newly created product
     {
-        Node[] memory nullNodeArray;
-        Product memory prod = Product(prodId[_id], nullNodeArray);
         uint256 prodCode = (_id * prodIndexOffset) + prodCount;
-        prodIndex[prodCode] = prod;
+        prodIndex[prodCode] = _id;
+        prodExist[prodCode] = true;
         prodCount++;
+    }
+    
+    function GetProductCode(uint256 _id) external view returns(uint256) //Gets the product code of the most recently created product
+    {
+        return (_id * prodIndexOffset) + (prodCount - 1);
+    }
 
-        emit ProductCreated(prodCode);
+    function GetSupplyChain(uint256 _prodCode) external view returns(Node[] memory) //Gets supply chain for the respective product code
+    {
+        return supplyChain[_prodCode];
+    }
+
+    function GetUser() external view returns(User memory) //Get user info
+    {
+        return users[msg.sender];
+    }
+
+    function Login(string memory _password) external view returns(bool) //Compares password entered to the password stored here
+    {
+        if(keccak256(abi.encodePacked(_password)) == keccak256(abi.encodePacked(password)))
+            return true;
+        else 
+            return false;
+    }
+
+    function UpdateSupplyChain(uint256 _prodCode, string memory _prodName, string memory _prodDesc) external //Adds a node to the supply chain for the respective product code
+    {
+        require(prodExist[_prodCode], "Invalid Product Code");
+
+        supplyChain[_prodCode].push(Node(users[msg.sender], _prodName, _prodDesc, msg.sender));
+    }
+
+    function UpdateUser(string memory _userType, string memory _name, string memory _location, string memory _userDesc) external //Update information of the user
+    {
+        User memory user = User(_userType, _name, _location, _userDesc);
+        users[msg.sender] = user;
     }
 }
--- backend/test/SupplyChain.js
+++ backend/test/SupplyChain.js
@@ -4,6 +4,22 @@ const {
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const BigNumber = ethers.BigNumber;
+
+const CompareObj = (_obj1, _obj2) => {
+  for(let i = 0; i < _obj2.length; i++)
+  {
+    if(_obj1[i] !== _obj2[i])
+      return false;
+    
+    return true;
+  }
+}
+
+const ConvertBigNumber = (_bigNum) => {
+  const bigNumber = BigNumber.from(_bigNum);
+  return bigNumber.toNumber();
+}
 
 describe("SupplyChain", function () {
   let supplyChain
@@ -14,10 +30,48 @@ describe("SupplyChain", function () {
     supplyChain = await supplyChainFactory.deploy();
   })
 
-  describe("Get Token Count",async()=>{
-    it("Should get token count",async function() {
-      let count = await supplyChain.GetCount();
-      expect(count).to.equal(1);
+  describe("Create New Product",async()=>{
+    it("Should create new product and increment product count",async function() {
+      expect(await supplyChain.prodCount()).to.equal(0);
+      await supplyChain.CreateProduct(1);
+      await supplyChain.CreateProduct(1);
+      expect(ConvertBigNumber(await supplyChain.GetProductCode(1))).to.equal(1000000001);
+      expect(await supplyChain.prodCount()).to.equal(2);
+    })
+  })
+
+  describe("Check Password",async()=>{
+    it("Should check if the password passed is equal to the stored password",async function() {
+      expect(await supplyChain.Login("$2a$12$REJHy2aPASYWEXUdVwjpZu5xbR2OTludsvq4iYoPap/k1UUbo5Sjy")).to.equal(true);
+    })
+  })
+
+  describe("Create New User",async()=>{
+    it("Should create new user and store info",async function() {
+      await supplyChain.connect(user1).UpdateUser("Supplier", "Elon Musk", "Florida #27", "Produces high quality pure butter");
+      const userInfo = await supplyChain.connect(user1).GetUser();
+      expect(CompareObj(userInfo, ["Supplier", "Elon Musk", "Florida #27", "Produces high quality pure butter"])).to.equal(true);
+    })
+  })
+
+  describe("Update Supply Chain",async()=>{
+    it("Should update the supply chain of the specific product",async function() {
+      await supplyChain.CreateProduct(1);
+      await supplyChain.connect(user1).UpdateUser("Supplier", "Elon Musk", "Florida #27", "Produces high quality pure butter");
+      await supplyChain.connect(user1).UpdateSupplyChain(1000000000, "Peanut Butter", "Packaged 100kg of butter");
+      await expect(supplyChain.connect(user1).UpdateSupplyChain(1000000002, "Peanut Butter", "Packaged 100kg of butter")).to.be.revertedWith('Invalid Product Code');
+    })
+  })
+
+  describe("Get Supply Chain",async()=>{
+    it("Should get info about the supply chain for specific product",async function() {
+      await supplyChain.CreateProduct(1);
+      await supplyChain.connect(user1).UpdateUser("Supplier", "Elon Musk", "Florida #27", "Produces high quality pure butter");
+      await supplyChain.connect(user1).UpdateSupplyChain(1000000000, "Peanut Butter", "Packaged 100kg of butter");
+      const info = await supplyChain.GetSupplyChain(1000000000);
+      const chainInfo = {type: info[0][0][0], owner: info[0][0][1], location: info[0][0][2], desc: info[0][0][3], prodName: info[0][1], processDesc: info[0][2], nodeAdd: info[0][3]};
+      console.log(chainInfo);
+      expect(chainInfo.nodeAdd).to.equal(user1.address);
     })
   })
 });
